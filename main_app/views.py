526ab9b97aaf3cefdab83357968956266cfe7f78
maniac-b8 <matthewdixon2539@gmail.com>
2024-05-28 13:00:05 -0700

    lab 1 done

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+finches = [
+  {'name': 'House finch', 'conservation_status': 'LC (Least Concern)', 'location': 'North America', 'age': 4},
+  {'name': 'Mangrove finch', 'conservation_status': 'CR (Critically Endangerd)', 'location': 'East Pacific Islands', 'age': 2},
+]
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
