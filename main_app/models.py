a4cde74cce076fb8abb55a0e90883f9f8576b022
maniac-b8 <matthewdixon2539@gmail.com>
2024-05-29 10:33:34 -0700

    lab 3 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Finch(models.Model):
@@ -12,3 +13,6 @@ class Finch(models.Model):
   # no makemigrations is necessary
   def __str__(self):
     return f'{self.name} ({self.id})'
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
