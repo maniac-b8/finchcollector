8329a8a6ee09594a70f7145a0ad3eaf834ac8999
maniac-b8 <matthewdixon2539@gmail.com>
2024-05-29 10:03:41 -0700

    lab 2 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  cons = models.CharField(max_length=100)
+  location = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  # Changing this instance method
+  # does not impact the database, therefore
+  # no makemigrations is necessary
+  def __str__(self):
+    return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
-finches = [
-  {'name': 'House finch', 'conservation_status': 'LC (Least Concern)', 'location': 'North America', 'age': 4},
-  {'name': 'Mangrove finch', 'conservation_status': 'CR (Critically Endangerd)', 'location': 'East Pacific Islands', 'age': 2},
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -14,6 +9,13 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
+  })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {
+    'finch' : finch
   })
