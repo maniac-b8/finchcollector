6c1b8299f35b8825441e5aad4f79e75f9e401204
maniac-b8 <matthewdixon2539@gmail.com>
2024-05-31 14:03:52 -0700

    lab 5 done

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,24 @@ MEALS = (
 )
 
 # Create your models here.
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   cons = models.CharField(max_length=100)
   location = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   # Changing this instance method
   # does not impact the database, therefore
